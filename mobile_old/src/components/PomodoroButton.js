b79017b349197325faa57da5bec3ed56d619e988
alejo95 <alejocobos95@gmail.com>
2026-05-11 11:13:24 +0200

    Refactor: Update version an dapted to environmant

---
+++ mobile_old/src/components/PomodoroButton.js
@@ -0,0 +1,16 @@
+import { TouchableOpacity, Text } from 'react-native';
+
+import { styles } from '../styles/appStyles';
+
+export default function PomodoroButton(props) {
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      onPress={props.onPress}
+    >
+      <Text style={styles.buttonText}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
